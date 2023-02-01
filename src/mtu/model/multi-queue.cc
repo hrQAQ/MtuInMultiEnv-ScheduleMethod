9dd1be5fa8cb2cc9a8117cac1ed9ed37a162a2d2
horik <hr.mail.qaq@gmail.com>
2023-02-01 17:57:18 +0800

    feat: SJF done

--- src/mtu/model/multi-queue.cc
+++ src/mtu/model/multi-queue.cc
@@ -18,14 +18,9 @@ namespace ns3
     }
 
     MultiQueue::MultiQueue()
-        : pri_number(3)
+        : pri_number(4)
     {
-        // NS_LOG_FUNCTION(this);
-        std::vector<double> weight;
-        weight.push_back(0.7);
-        weight.push_back(0.3);
-        m_weight = weight;
-        SetNumPriority(3, weight);
+        SetNumPriority(4);
     }
 
     MultiQueue::~MultiQueue()
@@ -33,26 +28,10 @@ namespace ns3
         // NS_LOG_FUNCTION(this);
     }
 
-    void MultiQueue::SetNumPriority(uint16_t number, std::vector<double> weight)
+    void MultiQueue::SetNumPriority(uint16_t number)
     {
 
-        //update the number of priorities
         pri_number = number;
-        uint16_t cp = number - 1;
-        //update the weight vector
-        if (number < 1)
-        {
-            // NS_LOG_ERROR("The number of prioroty queues cannot be less than 1");
-            return;
-        }
-        if (weight.size() != cp)
-        {
-            // NS_LOG_ERROR("Number of the weights is wrong");
-            return;
-        }
-        pri_number = number;
-        m_weight = weight;
-
         uint16_t current_number = m_queues.size();
         if (current_number < pri_number)
         {
@@ -60,26 +39,6 @@ namespace ns3
             {
                 std::queue<Ptr<Packet>> single_queue;
                 m_queues.push_back(single_queue);
-            }
-        }
-        else if (current_number > pri_number)
-        {
-            std::vector<Ptr<Packet>> vecTemp;
-            uint16_t indexStart = pri_number;
-            for (uint16_t i = indexStart; i < current_number; i++)
-            {
-                std::queue<Ptr<Packet>> queTemp = m_queues[i];
-                while (queTemp.size() > 0)
-                {
-                    vecTemp.push_back(queTemp.front());
-                    queTemp.pop();
-                }
-            }
-            m_queues.resize(pri_number);
-            std::queue<Ptr<Packet>> queBack = m_queues.back();
-            for (uint32_t i = 0; i < vecTemp.size(); i++)
-            {
-                queBack.push(vecTemp[i]);
             }
         }
     }
@@ -104,7 +63,7 @@ namespace ns3
             }
             else
             {
-                m_queues[priority].push(packet);
+                m_queues[priority + 1].push(packet);
                 return true;
             }
         }
@@ -112,8 +71,6 @@ namespace ns3
         {
             //if not found --> highest priority
             m_queues[0].push(packet);
-            // m_queues[m_queues.size() - 1].push(packet);
-
             return true;
         }
     } // namespace ns3
@@ -129,45 +86,18 @@ namespace ns3
         }
         else
         {
-            //no packets in highest priority
-            std::vector<int> nonEmpty;
             //pick non empty queues
-            for (uint16_t i = 1; i <= m_weight.size(); i++)
-            {
+            int idx = -1;
+            for (uint16_t i = 1; i < m_queues.size(); i++) {
                 if (m_queues[i].size() > 0)
                 {
-                    nonEmpty.push_back(i);
+                    idx = i;
                 }
             }
-            if (nonEmpty.size() == 0)
+            if (idx == -1)
                 return 0;
-
-            std::vector<double> vecAcc;
-            double acc = 0;
-            for (uint16_t i = 0; i < nonEmpty.size(); i++)
-            {
-                acc += m_weight[nonEmpty[i]];
-                vecAcc.push_back(acc);
-            }
-            //normalize
-            for (uint16_t i = 0; i < vecAcc.size(); i++)
-            {
-                vecAcc[i] /= acc;
-            }
-            double randomValue = MtuUtility::gen_random();
-            uint32_t index;
-            for (uint16_t i = 0; i < vecAcc.size(); i++)
-            {
-                index = i;
-                if (randomValue < vecAcc[i] && m_queues[i].size() > 0)
-                {
-                    break;
-                }
-            }
-
-            uint32_t que_index = nonEmpty[index];
-            packet = m_queues[que_index].front();
-            m_queues[que_index].pop();
+            packet = m_queues[idx].front();
+            m_queues[idx].pop();
         }
         return packet;
     }
